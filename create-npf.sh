7600a1e95240b22bd83df2c924fe7cf43bb3f054
lennard.boediger <lennard@nitrokey.com>
2021-08-14 17:30:31 +0200

    add create-npf.sh

--- create-npf.sh
+++ create-npf.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-TAG=$(git describe --tags --dirty)
+set -exuo pipefail
+HEADS_GIT_VERSION=$(git describe --tags)
 BOARD=$1
-cd ./build/BOARD/
+cd ./build/${BOARD}/
 sha256sum heads-${BOARD}-${HEADS_GIT_VERSION}.rom > sha256sum
 zip heads-${BOARD}-${HEADS_GIT_VERSION}.npf heads-${BOARD}-${HEADS_GIT_VERSION}.rom sha256sum
