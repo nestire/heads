2660e319c57b4bb7000996389288181567120542
Markus Meissner <coder@safemailbox.de>
2022-03-02 14:32:41 +0100

    update create-npf.sh to output sha256sum.txt (#12)

--- create-npf.sh
+++ create-npf.sh
@@ -3,5 +3,5 @@ set -exuo pipefail
 HEADS_GIT_VERSION=$(git describe --tags)
 BOARD=$1
 cd ./build/${BOARD}/
-sha256sum heads-${BOARD}-${HEADS_GIT_VERSION}.rom > sha256sum
-zip heads-${BOARD}-${HEADS_GIT_VERSION}.npf heads-${BOARD}-${HEADS_GIT_VERSION}.rom sha256sum
+sha256sum heads-${BOARD}-${HEADS_GIT_VERSION}.rom > sha256sum.txt
+zip heads-${BOARD}-${HEADS_GIT_VERSION}.npf heads-${BOARD}-${HEADS_GIT_VERSION}.rom sha256sum.txt
